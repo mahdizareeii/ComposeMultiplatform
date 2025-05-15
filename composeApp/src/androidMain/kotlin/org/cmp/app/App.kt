ac8adb8d83767c254d39c918aca014a400557616
mahdizareei <m.zareei@jabama.com>
2025-05-16 01:02:28 +0330

    create screen shots

--- composeApp/src/androidMain/kotlin/org/cmp/app/App.kt
+++ composeApp/src/androidMain/kotlin/org/cmp/app/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.cmp.app.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
+//this class can separate from this shared module and just include the shared cmp module in the own module
 class App : Application() {
 
     override fun onCreate() {
--- composeApp/src/androidMain/kotlin/org/cmp/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/cmp/app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+//this class can separate from this shared module and just include the shared cmp module in the own module
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
